f630b0b5a74fc00850f5c50c4513b6666d4dba22
Arlyn <arlyn.goodrich@gmail.com>
2021-03-22 19:12:14 -0500

    Initial commit

---
+++ Source/SRPG/SRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SRPG, "SRPG" );
---
+++ Source/SRPG/SRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SRPG_API ASRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
